a0d1c1dbd791f0a5cc0c32d99244a973204547cd
Himanshi Tiwari <himanshi@deepspace9.tech>
2025-12-07 22:51:34 +0530

    added react-email package

--- src/app/api/me/address/route.ts
+++ src/app/api/me/address/route.ts
@@ -1,2 +1,9 @@
 // src/app/api/me/address/route.ts
-export { GET, PUT, runtime } from "../profile/route";
+// export { GET, PUT, runtime } from "../profile/route";
+import { GET as profileGET, PUT as profilePUT } from "../profile/route";
+
+export const GET = profileGET;
+export const PUT = profilePUT;
+
+export const runtime = "edge";
+
