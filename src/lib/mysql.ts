7bc6b3593edcd13a07aeae4218bb7fcbd5c84df8
Himanshi Tiwari <himanshi@deepspace9.tech>
2026-04-15 00:47:47 +0530

    updated default port

--- src/lib/mysql.ts
+++ src/lib/mysql.ts
@@ -2,13 +2,14 @@ import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT || 4000),
+  port: Number(process.env.MYSQL_PORT || 3306),
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
 
   ssl: {
-    minVersion: "TLSv1.2"
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: false,
   },
 
   waitForConnections: true,
